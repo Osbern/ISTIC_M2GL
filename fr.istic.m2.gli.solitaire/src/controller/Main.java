5518bc6ca89b3a5085bcc07955fc46d0df01404a
xiorcal <xiorcal@gmail.com>
2012-11-22 08:48:09 +0100

    [GLI] ihm

--- fr.istic.m2.gli.solitaire/src/controller/Main.java
+++ fr.istic.m2.gli.solitaire/src/controller/Main.java
@@ -5,6 +5,8 @@ import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
+import presentation.PSolitaire;
+
 import solitaire.application.Solitaire;
 
 public class Main {
@@ -13,32 +15,16 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		CUsine u = new CUsine();
-		 Solitaire jeu = new Solitaire("Solitaire", u);
-		 jeu.initialiser();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		//frame.setLayout(new FlowLayout()); // au lieu de BorderLayout par défaut
-		
+		// frame.setLayout(new FlowLayout()); // au lieu de BorderLayout par
+		// dï¿½faut
+
 		CSabot cs = new CSabot("Sabot", u);
-		
+
 		CTasDeCartes ctdc = new CTasDeCartes("toto", u);
 		for (int c = 1; c < 14; c++) {
 			CCarte cc = new CCarte(c, 1);
@@ -46,74 +32,75 @@ public class Main {
 		}
 		ctdc.getPresentation().setDelta(0, 25);
 		cs.setReserve(ctdc);
-		
-		//frame.add(cs.getPresentation());
+
+		// frame.add(cs.getPresentation());
 
 		/*
 		 * test des PTasDeCartes on affiche un tas alternees vertical
 		 */
 		CTasDeCartesAlternees tas = new CTasDeCartesAlternees("toto", u);
 		tas.getPresentation().setDelta(0, 25);
-		
+
 		CCarte c = new CCarte(13, 4);
 		tas.empiler(c);
 		c = new CCarte(12, 3);
 		tas.empiler(c);
 		c = new CCarte(11, 2);
 		tas.empiler(c);
-		
-		//frame.add(tas.getPresentation());
-		
+
+		// frame.add(tas.getPresentation());
+
 		/*
 		 * test des PTasDeCartes on affiche un tas colorees vertical
 		 */
 		CTasDeCartesColorees tasc = new CTasDeCartesColorees("toto", 4, u);
 		tasc.getPresentation().setDelta(0, 4);
-		
+
 		c = new CCarte(1, 4);
 		tasc.empiler(c);
 		c = new CCarte(2, 4);
 		tasc.empiler(c);
 		c = new CCarte(3, 4);
 		tasc.empiler(c);
-		
-		//frame.add(tasc.getPresentation());
-		
+
+		// frame.add(tasc.getPresentation());
+
 		/*
 		 * test des PColonne
 		 */
 		CTasDeCartes pastas = new CTasDeCartes("toto", u);
 		pastas.getPresentation().setDelta(0, 25);
-		
+
 		c = new CCarte(13, 4);
 		pastas.empiler(c);
 		c = new CCarte(12, 3);
 		pastas.empiler(c);
 		c = new CCarte(11, 2);
 		pastas.empiler(c);
-		
+
 		CColonne col = new CColonne("toto", u);
 		col.setReserve(pastas);
 		col.empiler(new CCarte(13, 1));
 		col.empiler(new CCarte(12, 4));
 		col.empiler(new CCarte(11, 3));
-		
+
 		frame.add(col.getPresentation());
-		
+
 		/*
-		 * tests de PCarte on affiche toutes les cartes dont certaines face cachée!
+		 * tests de PCarte on affiche toutes les cartes dont certaines face
+		 * cachï¿½e!
 		 */
-//		CCarte pc;
-//		for (int c = 1; c < 5; c++) {
-//			for (int i = 1; i < 14; i++) {
-//				pc = new CCarte(i, c);
-//				pc.setFaceVisible(true);
-//				pc.getPresentation().setLocation(i, c);
-//				frame.add(pc.getPresentation());
-//				if ((i%4) == 0)
-//					pc.setFaceVisible(false);
-//			}
-//		}
+		// CCarte pc;
+		// for (int c = 1; c < 5; c++) {
+		// for (int i = 1; i < 14; i++) {
+		// pc = new CCarte(i, c);
+		// pc.setFaceVisible(true);
+		// pc.getPresentation().setLocation(i, c);
+		// frame.add(pc.getPresentation());
+		// if ((i%4) == 0)
+		// pc.setFaceVisible(false);
+		// }
+		// }
 
 		frame.setVisible(true);
 		frame.pack();
--- fr.istic.m2.gli.solitaire/src/presentation/Psolitaire.java
+++ fr.istic.m2.gli.solitaire/src/presentation/Psolitaire.java
@@ -1,27 +1,95 @@
 package presentation;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
-public class Psolitaire extends JFrame {
+import solitaire.application.Solitaire;
 
-	private JPanel contentPane;
+import controller.CColonne;
+import controller.CSabot;
+import controller.CSolitaire;
+import controller.CTasDeCartesColorees;
+import controller.CUsine;
 
-	
+public class PSolitaire extends JFrame {
+	public static void main(String[] args) {
+
+		CUsine u = new CUsine();
+		Solitaire jeu = new Solitaire("Solitaire", u);
+		jeu.initialiser();
+		PSolitaire ps = new PSolitaire((CSolitaire) jeu);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private CSolitaire csolitaire;
+
 	/**
 	 * Create the frame.
 	 */
-	public Psolitaire() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
+	public PSolitaire(CSolitaire s) {
+		this.csolitaire = s;
+		JFrame board = new JFrame();
+		board.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		board.setTitle("Solitaire - Destouches Lacroix");
+
+		board.setLayout(new BorderLayout());
+
+		CSabot sabot = csolitaire.getSabot();
+
+		JPanel north = new JPanel();
+		FlowLayout nfl = new FlowLayout(FlowLayout.LEADING);
+		north.setLayout(nfl);
+		north.add(sabot.getPresentation());
+
+		north.add(new ComposantVide());
+
+		CTasDeCartesColorees[] piles = csolitaire.getPiles();
+		for (CTasDeCartesColorees t : piles) {
+			t.getPresentation().setVisible(true);
+			north.add(t.getPresentation());
+		}
+		north.setSize(50 + (4 * 90) + sabot.getPresentation().getWidth(), 110);
+		north.setPreferredSize(north.getSize());
+
+		JPanel center = new JPanel();
+		center.setLayout(new FlowLayout());
+		CColonne[] cols = csolitaire.getCols();
+		for (CColonne c : cols) {
+			c.getPresentation().setVisible(true);
+			center.add(c.getPresentation());
+		}
+
+		center.setSize(7 * 90, (7 * 25) + (12 * 25) + 100);
+
+		board.add(north, BorderLayout.NORTH, 0);
+		center.setOpaque(false);
+		board.add(center, BorderLayout.CENTER, 0);
+
+		board.setSize(Math.max(center.getWidth(), north.getWidth()),
+				center.getHeight() + 5 + north.getHeight());
+		board.setPreferredSize(board.getSize());
+		board.setBackground(Color.RED);
+		board.setVisible(true);
 	}
 
+	class ComposantVide extends JPanel {
+		private static final long serialVersionUID = 1L;
+
+		ComposantVide() {
+			this.setSize(50, 50);
+			this.setPreferredSize(this.getSize());
+			Color tsp = new Color(0, 0, 0, 0);
+			this.setBackground(tsp);
+			this.setVisible(true);
+		}
+	}
 }
