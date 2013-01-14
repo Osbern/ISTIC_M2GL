fada7e99ff76f5664461ebf810c50fd220064a3d
Osbern <thibaud.destouches@gmail.com>
2013-01-14 11:51:22 +0100

    PlayNotes

--- fr.istic.m2.aoc.metronome/src/v1/Impl/UI/PlayNotes.java
+++ fr.istic.m2.aoc.metronome/src/v1/Impl/UI/PlayNotes.java
@@ -4,8 +4,6 @@ import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
 import com.jsyn.instruments.SubtractiveSynthVoice;
 import com.jsyn.unitgen.LineOut;
-import com.jsyn.unitgen.SawtoothOscillator;
-import com.jsyn.unitgen.SineOscillator;
 import com.jsyn.unitgen.UnitGenerator;
 import com.jsyn.unitgen.UnitVoice;
 import com.softsynth.shared.time.TimeStamp;
@@ -22,7 +20,7 @@ public class PlayNotes {
 	private UnitVoice voice;
 	private LineOut lineOut;
 
-	public void play(double freq) {
+	public PlayNotes() {
 		// Create a context for the synthesizer.
 		synth = JSyn.createSynthesizer();
 		synth.getAudioDeviceManager().setSuggestedOutputLatency(0.123);
@@ -41,7 +39,9 @@ public class PlayNotes {
 
 		// Start synthesizer using default stereo output at 44100 Hz.
 		synth.start();
+	}
 
+	public void play(double freq) {
 		// Get synthesizer time in seconds.
 		double timeNow = synth.getCurrentTime();
 
